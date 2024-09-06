88068400889b1e69eea40aec7dabbc2c823f977e
atiqur rahman <abulbasharatiqur@gmail.com>
2024-09-06 10:52:26 +0600

    organize code

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,35 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import prisma from "@/prisma/client";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+  console.log(issues);
   return (
     <div>
       <Link href="/issues/new">
-        <Button>Create New Issue</Button>
+        <div className="mb-3">
+          <Button>Create New Issue</Button>
+        </div>
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Actions</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {issues.map((issue) => (
+              <Table.Row key={issue.id}>
+                <Table.Cell>{issue.title}</Table.Cell>
+                <Table.Cell>{issue.status}</Table.Cell>
+                <Table.Cell>{issue.createdAt.toLocaleDateString()}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
       </Link>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
+import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
-import "./globals.css";
-import "./theme-config.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 import Navbar from "./Navbar";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import "./theme-config.css";
 
 const inter = Inter({
   subsets: ["latin"],
