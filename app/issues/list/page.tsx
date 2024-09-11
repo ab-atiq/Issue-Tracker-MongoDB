9e670ddfe9cc1630900ee3aa8d81a5c3c1ed574a
atiqur rahman <abulbasharatiqur@gmail.com>
2024-09-11 07:19:17 +0600

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -3,9 +3,23 @@ import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import delay from "delay";
 import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  // console.log(searchParams);
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
   // console.log(issues);
   await delay(1000);
 
