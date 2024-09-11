4197b3696ad4dec6b4d9d52c5cbcfed69578fb0e
atiqur rahman <abulbasharatiqur@gmail.com>
2024-09-11 11:44:38 +0600

    Dashboard - Latest Issue, Issue Summary and Charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import { Button } from "@radix-ui/themes";
-import { Pagination } from "@/app/components";
-export default function Home({
-  searchParams,
-}: {
-  searchParams?: { page: string };
-}) {
-  if (!searchParams?.page) searchParams = { page: "1" };
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
+import { Grid, Flex } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
+
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
   return (
-    <>
-      <div>Hello World!</div>
-      <Button>Radix UI button</Button>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-      />
-    </>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
   );
 }
